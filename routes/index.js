a4169ee47f670a629589011b5191a36e0894823a
kristenmazza <kbmazza@gmail.com>
2023-08-23 11:30:25 -0700

    Enable user-submitted messages

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,39 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const format = require('date-fns/format');
 
 const messages = [
   {
     text: 'Hi there!',
     user: 'Amando',
     added: new Date(),
+    format: format,
   },
   {
     text: 'Hello World!',
     user: 'Charles',
     added: new Date(),
+    format: format,
   },
 ];
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+router.post('/new', (req, res) => {
+  let messageText = req.body.message;
+  let messageUser = req.body.name;
+
+  messages.push({
+    text: messageText,
+    user: messageUser,
+    added: new Date(),
+    format: format,
+  });
+
+  res.redirect('/');
 });
 
 module.exports = router;
--- routes/new.js
+++ routes/new.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { messages } = require('./index');
 
 /* GET new message page. */
 router.get('/new', function (req, res, next) {
