e7493b0b0f3729df02cc74a943269e5bcd6088ff
kristenmazza <kbmazza@gmail.com>
2023-08-25 17:24:21 -0700

    Refactor route to appropriate file

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,4 @@ const message_controller = require('../controllers/messageController');
 /* GET home page. */
 router.get('/', message_controller.message_list);
 
-router.post('/new', message_controller.message_create_post);
-
 module.exports = router;
--- routes/new.js
+++ routes/new.js
@@ -5,4 +5,7 @@ const message_controller = require('../controllers/messageController');
 /* GET new message page. */
 router.get('/', message_controller.message_create_get);
 
+/* POST a new message */
+router.post('/', message_controller.message_create_post);
+
 module.exports = router;
